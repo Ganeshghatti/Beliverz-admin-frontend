66706a717824d1356d252d9800a854b81f6f5994
Ganeshghatti <ghattiganesh8@gmail.com>
2024-08-16 19:07:32 +0530

    change to vercel server

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEOZ11PjYsHwGM6cochFdn8upcCWhYRDM",
-  authDomain: "beliverzjrf-b5686.firebaseapp.com",
-  projectId: "beliverzjrf-b5686",
-  storageBucket: "beliverzjrf-b5686.appspot.com",
-  messagingSenderId: "196897464842",
-  appId: "1:196897464842:web:8e35401a991f871ac7b5f3"
+  apiKey: "AIzaSyD7PRg1qhuRP0CrtfOj-puIjfb_HmLn4tg",
+  authDomain: "beliverz-prod.firebaseapp.com",
+  projectId: "beliverz-prod",
+  storageBucket: "beliverz-prod.appspot.com",
+  messagingSenderId: "917347137280",
+  appId: "1:917347137280:web:1989f51e45f86a6e52e5e5"
 };
 
 export const app = initializeApp(firebaseConfig);
